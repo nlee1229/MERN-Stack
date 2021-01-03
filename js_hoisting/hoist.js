3e19f9da97c92a976e9ee69c15c71719b604fbeb
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-01-02 23:23:09 -0800

    completed hoist assign

--- js_hoisting/hoist.js
+++ js_hoisting/hoist.js
@@ -7,25 +7,25 @@ console.log(hello);
 hello = 'world';
 // prediction: undefined, because the 'var' variable is being hoisted
 
-// 2.
+2;
 // var needle = 'haystack';
 // test();
 // function test() {
 //   var needle = 'magnet';
 //   console.log(needle);
 // }
-//prediction: magnet, haystack----outcome: magnet
+// prediction: magnet, haystack----output: magnet
 
 var needle;
 function test() {
   var needle;
+  // value declaration
   needle = 'magnet';
   console.log(needle);
 }
 needle = 'haystack';
 test();
-console.log(needle);
-// prediction: magnet, haystack ---- outcome: magnet, haystack
+// prediction: magnet, haystack ---- output: magnet
 
 // 3.
 // var brendan = 'super cool';
@@ -37,16 +37,14 @@ console.log(needle);
 
 var brendan;
 function print() {
-  var brendan;
   brendan = 'only okay';
   console.log(brendan);
 }
 brendan = 'super cool';
-print();
 console.log(brendan);
-// prediction: only okay, super cool ---- outcome: only okay, super cool
+// prediction: only okay, super cool ---- output: super cool
 
-// 4. ??????????
+// 4.
 // var food = 'chicken';
 // console.log(food);
 // eat();
@@ -60,23 +58,85 @@ var food;
 function eat() {
   var food;
   food = 'half-chicken';
-  food = 'gone';
   console.log(food);
+  food = 'gone';
 }
 food = 'chicken';
-eat();
 console.log(food);
-// prediction: half-chicken, chicken
+eat();
+// prediction: chicken, half-chicken ---- output: chicken, half-chicken
 
-5;
+// 5;
+// mean();
+// console.log(food);
+// var mean = function () {
+//   food = 'chicken';
+//   console.log(food);
+//   var food = 'fish';
+//   console.log(food);
+// };
+// console.log(food);
+
+var mean;
 mean();
 console.log(food);
-var mean = function () {
+mean = function () {
+  var food;
   food = 'chicken';
   console.log(food);
-  var food = 'fish';
+  food = 'fish';
   console.log(food);
 };
 console.log(food);
+// prediction: chicken, fish ---- output: error, b/c mean is not a function
 
-var
+// 6;
+// console.log(genre);
+// var genre = 'disco';
+// rewind();
+// function rewind() {
+//   genre = 'rock';
+//   console.log(genre);
+//   var genre = 'r&b';
+//   console.log(genre);
+// }
+// console.log(genre);
+
+var genre;
+function rewind() {
+  var genre;
+  genre = 'rock';
+  console.log(genre);
+  genre = 'r&b';
+  console.log(genre);
+}
+console.log(genre);
+genre = 'disco';
+rewind();
+console.log(genre);
+// prediction: rock, r&b, disco ---- output: rock, r&b, disco
+
+7;
+dojo = 'san jose';
+console.log(dojo);
+learn();
+function learn() {
+  dojo = 'seattle';
+  console.log(dojo);
+  var dojo = 'burbank';
+  console.log(dojo);
+}
+console.log(dojo);
+
+var dojo;
+dojo = 'san jose';
+console.log(dojo);
+function learn() {
+  var dojo = 'burbank';
+  console.log(dojo);
+  dojo = 'seattle';
+  console.log(dojo);
+}
+learn();
+console.log(dojo);
+// prediction: san jose, brubank, seattle, san jose ---- output: san jose, brubank, seattle, san jose
