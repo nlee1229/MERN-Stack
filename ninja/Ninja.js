a82cf8cf39975c09303a503613072499b1b82085
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-01-03 22:59:00 -0800

    ninja

---
+++ ninja/Ninja.js
@@ -0,0 +1,28 @@
+class Ninja {
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+    this.speed = 3;
+    this.strength = 3;
+  }
+
+  sayName() {
+    console.log(`Your ninjas name is ${this.name}.`);
+  }
+
+  showStats() {
+    console.log(
+      `Your ninjas stats are... Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}.`
+    );
+  }
+
+  drinkSake() {
+    this.health += 10;
+    console.log(`Current health: ${this.health}.`);
+  }
+}
+
+const ninja1 = new Ninja('Hyabusa', 100);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
