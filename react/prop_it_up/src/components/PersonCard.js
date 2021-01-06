46d8d2b99a0c400e763f4a55e660da2ebdc99f9b
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-01-05 21:02:08 -0800

    Completed Putting It Together

--- react/prop_it_up/src/components/PersonCard.js
+++ react/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        // Use super whenever there is an  extends
+        super(props);
+        this.state = {
+            age: props.age
+        }
+    }
+
+    increaseAge = () => {
+        // this.state. age is coming from the age on line 8
+        this.setState({ age: this.state.age + 1 })
+    };
+
+    decreaseAge = () => {
+        this.setState({ age: this.state.age - 1 })
+    };
+
     render() {
         const { name, age, hairColor } = this.props;
         return (
             <div>
                 <h1>{name}</h1>
-                <h3>Age: {age}</h3>
+                <h3>Age: {this.state.age}</h3>
                 <h3>Hair Color: {hairColor}</h3>
+
+                <button onClick={this.increaseAge}>Happy Birthday!</button>
+                <button onClick={this.decreaseAge}>Click to gain a year!</button>
             </div>
         );
     }
