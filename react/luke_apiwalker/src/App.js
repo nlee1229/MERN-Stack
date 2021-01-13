ba720b61bf65ebde53c198f5624307ff35eb16ae
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-01-12 21:46:13 -0800

    started luke apiwalker

---
+++ react/luke_apiwalker/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LukeAPI from './components/LukeAPI.jsx';
+import { Router } from '@reach/router';
+
+function App() {
+  return (
+    <div className="App">
+      <LukeAPI />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/luke_apiwalker/src/components/LukeAPI.jsx
@@ -0,0 +1,8 @@
+function LukeAPI() {
+    return (
+        <h1>Hello from Starwars</h1>
+
+    );
+}
+
+export default LukeAPI;
