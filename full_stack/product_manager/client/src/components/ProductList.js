d6578ddae6ce12b51f983f66addafba7c30eb82b
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-01-18 16:15:19 -0800

    completed Product Manager pt.2

--- full_stack/product_manager/client/src/components/ProductList.js
+++ full_stack/product_manager/client/src/components/ProductList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+
 export default props => {
     return (
         <div>
             <h1>All Products:</h1>
             {props.products.map((product, idx) => {
-                return <p key={idx}>{product.title}, {product.price}, {product.description}</p>
+                return <p key={idx}><a href={`/products/${product._id}`}> {product.title}, {product.price}, {product.description}</a></p>
             })}
-        </div>
+        </div >
     )
 }
+
--- full_stack/product_manager/server/routes/product.routes.js
+++ full_stack/product_manager/server/routes/product.routes.js
@@ -3,7 +3,10 @@ const ProductController = require('../controllers/product.controller');
 // when we go to /api, we want to run this index method in the controller
 module.exports = function (app) {
     app.get('/api', ProductController.index);
+    // ----- PATH TO CREATE NEW PRODUCT ----- //
     app.post('/api/products', ProductController.createProduct);
+    // ----- PATH TO SHOW ALL PRODUCTS ----- //
     app.get('/api/products', ProductController.getAllProducts);
+    // ----- PATH TO SHOW ONE PRODUCT BY ID ----- //
     app.get('/api/products/:id', ProductController.getProduct);
 }
